da82eefcbbaace799fe906734a4fe8549f5df54d
Chazidy <williamc2811@student.faytechcc.edu>
2025-01-27 18:47:24 +0000

    writing things about me

---
+++ M1T2_Williams.cpp
@@ -0,0 +1,22 @@
+/*starting coding with c++
+module 1
+Chazidy Williams
+1/27/25
+*/
+
+# include <iosstream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello" << endl;
+    cout << "My name is chazidy" << endl;
+    cout << "I am 17 years old turning 18 on the 14th of february" << endl;
+    cout << "I enjoy watching my favorite show You " << endl;
+    cout << "My favorite food is nachos!! " << endl;
+    cout << endl;
+    
+    return 0
+
+}
