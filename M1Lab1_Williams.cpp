79c605ec9363a842ba0e3eb0911162a29b9ebb0a
Chazidy <williamc2811@student.faytechcc.edu>
2025-01-27 19:34:12 +0000

    donesy

--- M1Lab1_Williams.cpp
+++ M1Lab1_Williams.cpp
@@ -4,22 +4,28 @@
 // 1-27-25
 // Today we're selling apples
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
     //variables
+    string name = "chazs";
     int num_apples = 100;
-    string name = "chazidy";
     double price_each = 0.25;
 
+    //calculations
+    double total = num_apples * price_each;
+
+
     // welcome
-    cout <<" Welcome to the " << name << " apple farm." << endl;
+    cout <<"Welcome to the " << name << " apple farm." << endl;
     cout << "We have " << num_apples << " apples in stock." << endl;
     cout << "Price per apple is: $" << price_each << endl;
     cout << endl;
+   
 
-    //Calculate total
     //Print answer
+    cout << "The total answer " << num_apples<<" Apples is $" << total; << endl;
     return 0;
 }
