259ea2e76487483ba34feb107a769f9b2bb6561e
Chazidy <williamc2811@student.faytechcc.edu>
2025-02-10 14:31:43 -0500

    Add files via upload

---
+++ M2Lab.Williams.py
@@ -0,0 +1,31 @@
+from turtle import *
+
+screen = Screen()
+screen.bgcolor("lightyellow3")
+
+t = Turtle()
+t.shape("turtle")
+t.color("IndianRed4")
+t.pensize(3)
+t.pencolor("LavenderBlush")
+
+
+t.begin_fill()
+t.fillcolor('LavenderBlush')
+t.circle(100)
+t.end_fill()
+
+
+t.begin_fill()
+t.pencolor('Salmon4')
+t.forward(140)
+t.right(135)
+t.forward(250)
+t.right(118)
+t.forward(190)
+t.right(109)
+t.forward(100)
+t.fillcolor('Salmon4')
+t.end_fill()  
+
+
