e567e49ef62164a8a1a17c599c1f8a56d1a212fd
Chazidy <williamc2811@student.faytechcc.edu>
2025-02-16 04:48:37 +0000

    M2HW1

---
+++ M2HW1_Williams_Q4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Define variables
+    string letsGo = "Let's go ";
+    string school = "FTCC";
+    string team = "Trojans";
+
+    // Concatenate strings to build cheer messages
+    string cheerOne = letsGo + school;
+    string cheerTwo = letsGo + team;
+
+    // Print the cheers
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerTwo << endl;
+
+    return 0;
+}
