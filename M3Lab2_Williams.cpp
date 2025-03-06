08467306e33ea676482273d850afd78362fc787c
Chazidy <williamc2811@student.faytechcc.edu>
2025-03-06 01:40:14 +0000

    added while loop

--- M3Lab2_Williams.cpp
+++ M3Lab2_Williams.cpp
@@ -10,29 +10,46 @@ using namespace std;
 
 int main() {
 int numberGrade;
+char anotherGrade;
+
+do 
+{
 cout << "Enter a numerical grade (a number)" << endl;
 cin >> numberGrade;
 
 if (numberGrade >= 90)
 {
     cout << "Grade A" << endl;
+    cout << endl;
 }
 else if (numberGrade >= 80)
 {
     cout << "Grade B" << endl;
+    cout << endl;
 }
-else (numberGrade >= 70)
+else if (numberGrade >= 70)
 {
     cout << "Grade C" << endl;
+    cout << endl;
 }
-else (numberGrade >= 60)
+else if  (numberGrade >= 60)
 {
     cout << "Grade D" << endl;
+    cout << endl;
 }
 else 
 {
     cout << "Grade F" << endl;
+    cout << endl;
 }
+
+cout << "Would you like to enter another grade? (y/n): ";
+cin >> anotherGrade;
+
+}
+while (anotherGrade == 'y' || anotherGrade == 'Y');  // Continue if 'y' or 'Y'
+
+cout << "Thank you for using the grade converter!" << endl;
 return 0;
 
 }
