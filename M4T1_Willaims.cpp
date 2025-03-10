ad721bc5f097084bcf1dbb791f00e070b8ef97c5
Chazidy <williamc2811@student.faytechcc.edu>
2025-03-10 17:18:32 +0000

    Starting M4T1

---
+++ M4T1_Willaims.cpp
@@ -0,0 +1,20 @@
+ //CSC134
+ // M4T1 - While loops 
+ // Chazidy 
+ // 3/10/25
+
+ #include <iostream>
+  
+ using namespace std;
+   
+ int main()
+ {
+    // part 1 - just count
+    int count = 1;
+    
+    while(count < 5){
+        cout << "Hello #" << count << endl;
+    }
+    return 0;
+ 
+ }
